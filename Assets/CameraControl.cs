e6f639c639c7be8b782dc435030f9c3ad352216a
ziyanfadilla <ziyanfadilla1@gmail.com>
2021-04-18 17:14:59 +0700

    uts GameDesign

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+	public Transform PlayerTransform;
+	private Vector3 _cameraOffset;
+
+
+	[SerializeField]
+	public float SmothFactor = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		_cameraOffset = transform.position - PlayerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPost = PlayerTransform.position + _cameraOffset;
+		transform.position = Vector3.Slerp (transform.position,newPost,SmothFactor);
+
+
+		if (Input.GetKey (KeyCode.W)){
+			transform.Rotate(new Vector3(-30,0,0) * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)){
+			transform.Rotate(new Vector3(30,0,0) * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)){
+			transform.Rotate(new Vector3(0,-30,0) * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)){
+			transform.Rotate(new Vector3(0,30,0) * Time.deltaTime);
+		}
+		
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	public float  kecepatan = 50f;
+	private Rigidbody rb;
+	private int point;
+	public Text pointText;
+
+	// Use this for initialization
+	void Start () {
+		rb =  GetComponent<Rigidbody>();
+		point = 0;
+		SetCountText ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)){
+			this.gameObject.GetComponent<Rigidbody> ().AddForce(Vector3.up * kecepatan);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			this.gameObject.GetComponent<Rigidbody> ().AddForce(Vector3.left * kecepatan);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			this.gameObject.GetComponent<Rigidbody> ().AddForce(Vector3.right * kecepatan);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			this.gameObject.GetComponent<Rigidbody> ().AddForce(Vector3.back * kecepatan);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			this.gameObject.GetComponent<Rigidbody> ().AddForce(Vector3.back * -kecepatan);
+		}
+		
+
+		//transform.Translate(kecepatan*Input.GetAxis("Horizontal") * Time.deltaTime,0.0f,kecepatan * Input.GetAxis("Vertical") * Time.deltaTime);
+		
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag("Enemy")){
+			other.gameObject.SetActive (false);
+			point = point + 1;
+			SetCountText ();
+		}
+	}
+	void SetCountText(){
+		pointText.text = " Point : " +point.ToString ();
+	}
+}
